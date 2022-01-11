e45a4ba9628b0afbc1eb095c7ae638e5bd97d1c4
Vladislav Titov <capt.Hoffmann@gmail.com>
2022-01-11 21:47:13 +0300

    Add project files.

---
+++ Core.RabbitMQ.Library/Context/IRabbitMqContext.cs
@@ -0,0 +1,13 @@
+﻿using RabbitMQ.Client;
+
+namespace Core.RabbitMQ.Library.Context
+{
+    public interface IRabbitMqContext
+    {
+        IConnection PublisherConnection { get; }
+
+        IConnection SubscriberConnection { get; }
+
+        IConnection CreateNewRabbitConnection(string userName, string password, int port, string virtualHost, string hostName);
+    }
+}
